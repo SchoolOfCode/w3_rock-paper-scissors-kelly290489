f88934c83f16449368692a2b58ef5b627da55696
kelly290489 <kellyryan89@live.co.uk>
2021-04-27 07:14:31 +0100

    console works but not dom

--- main.js
+++ main.js
@@ -1,10 +1,40 @@
-let userChoice = prompt("Do you choose rock, paper or scissor?")
-let playerMove = "rock";
-let computerMove = "paper";
-if (playerMove === "rock" && computerMove === "paper")
-console.log("Paper Wins")
-
-let rock = paper;
-let paper = scissors;
-
-if (paper === )
+let userChoice = prompt("Do you choose rock, paper or scissors?");
+let computerChoice = Math.random();
+if (computerChoice < 0.34) {
+	computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+	computerChoice = "paper";
+} else {
+	computerChoice = "scissors";
+}
+let compare=function(choice1,choice2){
+    if(choice1===choice2){
+        return "The result is a tie!";
+    }
+    else if(choice1==="rock"){
+        if(choice2==="scissors"){
+            return "rock wins"
+        }
+        else if(choice2==="paper"){
+            return "paper wins";
+        }
+    }
+    else if(choice1==="paper"){
+        if(choice2==="scissors"){
+            return "scissors win"
+        }
+        else if(choice2==="rock"){
+            return "paper wins";
+        }
+    }
+    else if(choice1==="scissors"){
+        if(choice2==="paper"){
+            return "scissors win"
+        }
+        else if(choice2==="rock"){
+            return "rock wins";
+        }
+    }
+    
+}
+console.log(compare(userChoice,computerChoice));
